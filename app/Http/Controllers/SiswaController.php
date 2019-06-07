74bb57789867dd5fb25cfcf48acc892edb2eef96
ryanuarmd <ryanuarmd@gmail.com>
2019-06-07 13:31:58 +0700

    Update Controller

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -9,6 +9,6 @@ class SiswaController extends Controller
     public function index()
     {
     	$data_siswa = \App\Siswa::all();
-    	return view('siswa.index');
+    	return view('siswa.index',['data_siswa' => $data_siswa]);
     }
 }
